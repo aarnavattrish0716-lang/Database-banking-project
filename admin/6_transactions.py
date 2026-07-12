213ba77a32961546e60f12969ad1ae9f3bad7ec6
aarnavattrish0716-lang <aarnavattrish0716@gmail.com>
2026-07-12 22:56:31 +0530

    Updated admin/transaction

--- admin/6_transactions.py
+++ admin/6_transactions.py
@@ -1,9 +1,24 @@
 import streamlit as st
+import pandas as pd
 import modules.admin_fun as md
 st.title("📄 Transaction Management")
 st.divider()
 st.subheader("📋 Transaction History")
-data=md.get_all_transactions()
-st.dataframe(data,hide_index=True)
+df=pd.DataFrame(md.get_all_transactions())
 st.divider()
 st.subheader("🔍 Search / Filter")
+with st.container(border=True):
+    col1,col2=st.columns(2)
+    with col1:
+        search_name=st.text_input("Search Customer by name")
+    with col2:
+        search_account_id=st.text_input("Search by account_id")#Input type is str
+if search_name:
+    filtered=df[df["user_name"].str.lower().str.startswith(search_name.lower())]
+    st.dataframe(filtered,hide_index=True,width="stretch")
+elif search_account_id:
+    filtered=df[df["account_id"].astype(str).str.startswith(search_account_id)]
+    st.dataframe(filtered,hide_index=True,width="stretch")
+else:
+    filtered=df
+    st.dataframe(filtered,hide_index=True,width="stretch")
